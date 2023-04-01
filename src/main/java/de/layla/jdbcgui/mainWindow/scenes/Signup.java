9aa0c5d33f1b57b73ac4e6b89a4e7c7ce293c60c
lejler <layla.shokri@gmx.de>
2023-04-01 16:26:42 +0200

    added signup scene

--- src/main/java/de/layla/jdbcgui/mainWindow/scenes/Signup.java
+++ src/main/java/de/layla/jdbcgui/mainWindow/scenes/Signup.java
@@ -1,4 +1,20 @@
 package de.layla.jdbcgui.mainWindow.scenes;
 
-public class Signup {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+
+public class Signup extends GridPane {
+
+    public Signup() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("SignupView.fxml"));
+            loader.setRoot(this);
+            loader.load();
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+
 }
