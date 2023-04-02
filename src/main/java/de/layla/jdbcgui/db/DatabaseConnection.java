3a1a3a4efa5364e268dbebe670225958eb24cd78
lejler <layla.shokri@gmx.de>
2023-04-02 13:52:23 +0200

    added signup

--- src/main/java/de/layla/jdbcgui/db/DatabaseConnection.java
+++ src/main/java/de/layla/jdbcgui/db/DatabaseConnection.java
@@ -2,6 +2,7 @@ package de.layla.jdbcgui.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
@@ -16,7 +17,29 @@ public class DatabaseConnection {
         }
     }
 
-    public Connection connectAsRoot() {
+    public boolean createNewUser(String username, String password) {
+        try {
+            Connection connection = connectAsRoot();
+            String createUser = "CREATE USER ?@localhost IDENTIFIED BY ?;";
+            assert connection != null;
+            PreparedStatement createUserStatement = connection.prepareStatement(createUser);
+            createUserStatement.setString(1, username);
+            createUserStatement.setString(2, password);
+            createUserStatement.execute();
+
+            String grantPermissions = "GRANT ALL PRIVILEGES ON versand.* To ?@'localhost' IDENTIFIED BY ?;";
+            PreparedStatement grantPermissionsStatement = connection.prepareStatement(grantPermissions);
+            grantPermissionsStatement.setString(1, username);
+            grantPermissionsStatement.setString(2, password);
+            grantPermissionsStatement.execute();
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    private Connection connectAsRoot() {
         try {
             String user = "root";
             String password = "";
