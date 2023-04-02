bb277a93cd3e6c3a886888b4b5c26408363cc58d
lejler <layla.shokri@gmx.de>
2023-04-02 13:29:19 +0200

    added login

--- src/main/java/de/layla/jdbcgui/mainWindow/scenes/LoginController.java
+++ src/main/java/de/layla/jdbcgui/mainWindow/scenes/LoginController.java
@@ -1,17 +1,24 @@
 package de.layla.jdbcgui.mainWindow.scenes;
 
+import de.layla.jdbcgui.db.DatabaseConnection;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Popup;
+import javafx.stage.Window;
 
 import java.net.URL;
+import java.sql.Connection;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
 
+    private final DatabaseConnection databaseConnection = new DatabaseConnection();
+    private final Dialog<String> dialog = new Dialog<>();
+
+    @FXML
+    private GridPane root;
     @FXML
     private Label usernameLabel;
     @FXML
@@ -27,12 +34,19 @@ public class LoginController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        this.dialog.setHeaderText("Warning");
+        this.dialog.setContentText("User not found");
+        dialog.setResizable(false);
+        Window window = dialog.getDialogPane().getScene().getWindow();
+        window.setOnCloseRequest(e -> window.hide());
     }
 
     @FXML
     private void login() {
-
+        Connection connection = databaseConnection.connect(usernameField.getText(), passwordField.getText());
+        if (connection == null) {
+            dialog.showAndWait();
+        }
     }
 
     @FXML
