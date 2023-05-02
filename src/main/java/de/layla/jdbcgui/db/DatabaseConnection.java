7bf4e519d236d617c0c8440cd7c24b5f1d7166d1
lejler <layla.shokri@gmx.de>
2023-05-02 20:15:55 +0200

    user with same name cant be created more than once now

--- src/main/java/de/layla/jdbcgui/db/DatabaseConnection.java
+++ src/main/java/de/layla/jdbcgui/db/DatabaseConnection.java
@@ -1,9 +1,6 @@
 package de.layla.jdbcgui.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseConnection {
 
@@ -20,23 +17,29 @@ public class DatabaseConnection {
     public boolean createNewUser(String username, String password) {
         try {
             Connection connection = connectAsRoot();
-            String createUser = "CREATE USER ?@localhost IDENTIFIED BY ?;";
-            assert connection != null;
-            PreparedStatement createUserStatement = connection.prepareStatement(createUser);
-            createUserStatement.setString(1, username);
-            createUserStatement.setString(2, password);
-            createUserStatement.execute();
 
-            String grantPermissions = "GRANT ALL PRIVILEGES ON versand.* To ?@'localhost' IDENTIFIED BY ?;";
-            PreparedStatement grantPermissionsStatement = connection.prepareStatement(grantPermissions);
-            grantPermissionsStatement.setString(1, username);
-            grantPermissionsStatement.setString(2, password);
-            grantPermissionsStatement.execute();
-            connection.close();
-            return true;
+            assert connection != null;
+
+            if (!userExists(connection, username)) {
+                String createUser = "CREATE USER ?@localhost IDENTIFIED BY ?;";
+                assert connection != null;
+                PreparedStatement createUserStatement = connection.prepareStatement(createUser);
+                createUserStatement.setString(1, username);
+                createUserStatement.setString(2, password);
+                createUserStatement.execute();
+
+                String grantPermissions = "GRANT ALL PRIVILEGES ON versand.* To ?@'localhost' IDENTIFIED BY ?;";
+                PreparedStatement grantPermissionsStatement = connection.prepareStatement(grantPermissions);
+                grantPermissionsStatement.setString(1, username);
+                grantPermissionsStatement.setString(2, password);
+                grantPermissionsStatement.execute();
+                connection.close();
+                return true;
+            }
         } catch (SQLException e) {
             return false;
         }
+        return false;
     }
 
     private Connection connectAsRoot() {
@@ -47,5 +50,25 @@ public class DatabaseConnection {
         } catch (SQLException e) {
             return null;
         }
+    }
+
+    private boolean userExists(Connection connection, String username) {
+        try {
+            String userLookup = "SELECT USER FROM mysql.user WHERE USER = ?";
+            PreparedStatement statement = connection.prepareStatement(userLookup);
+            statement.setString(1, username);
+            ResultSet rs = statement.executeQuery();
+
+            int count = 1;
+
+            while (rs.next()) {
+                if (rs.getString(count).equals(username)) {
+                    return false;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
     }
 }
